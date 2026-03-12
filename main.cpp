5ae0c9608d980064b5d30abbe6d45df588a90479
trailblazer887 <2878371358@qq.com>
2026-03-12 11:02:13 +0800

    添加窗口图标

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QDebug>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 
     MainWindow w;
     w.setWindowTitle("音乐播放器");
+    w.setWindowIcon(QIcon("musicicon.png"));
     w.resize(847, 558);
     w.show();
 
